e7bfc4196d9e2e1aece366ecfa7f521a8f35ba8a
Hahabomba <adrianoveray@gmail.com>
2025-03-04 10:11:56 +0200

    buh

--- cluless.java
+++ cluless.java
@@ -1,6 +1,14 @@
 public class cluless
 {
+    private int balls;
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public cluless() {
+        this.balls = 2;
+    }
+
+
 }
